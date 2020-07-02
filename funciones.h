017b03055cf796ea95987e6ea577f8be1d1b107d
Cristian Uhrig <cristianuhrig@gmail.com>
2020-07-02 19:08:30 -0300

    se ordeno codigo y se sumaron funciones
    se paso la cabecera a forma del juego a forma de funcion y se corrigio que el juego tenga 10 rondas y 3 tiros por ronda

--- funciones.h
+++ funciones.h
@@ -17,4 +17,5 @@ void tirarDados(int [], int, int);
 void cambiarDados(int [], int);
 int buscarDadosRepetidos(int [], int, int );///cuenta la cantidad de veces que se repite un //número en el vector
 int calcularPuntaje(int [], int);
+void cabeceraJuego(int[], int, int, int, int, char[]);
 #endif // FUNCIONES_H_INCLUDED
